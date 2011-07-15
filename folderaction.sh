9490cc97a7ce0cf5f818c5ba677a3812040554f1
Juha Autero <jautero@iki.fi>
2011-07-16 01:30:36 +0300

    Added "don't backup" attribute to .virus file.

--- folderaction.sh
+++ folderaction.sh
@@ -13,6 +13,10 @@ do
     rc=$?
     if [ $rc -eq 3 ] ; then
 	echo "WARNING: malware found in the following file(s): $f" | gn -s "Scan result"
+	if [ -e $f.virus ] ; then
+	    # rename successfull, don't backup malware.
+	    xattr -w com.apple.metadata:com_apple_backup_excludeItem com.apple.backupd $f.virus
+	fi
     elif [ $rc -eq 0 ] ; then 
 	echo "Yay! No malware found in $f" | gn "Scan result"
     else
